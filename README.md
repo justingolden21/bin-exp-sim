7ab2fd4255e3eadb6c49a9ec484b1527835fcc60
RGB Studios <justingolden21@gmail.com>
2019-02-17 03:33:20 -0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Binomial Experiment Simulator
---
+++ scripts.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$('#simulateButton').click(function() {
+		// todo: error checking
+		let p = $('#pInput').val();
+		let n = $('#nInput').val();
+
+		let numSuccess = 0, numFailure = 0;
+		let boolArr = [];
+		for(let i=0; i<n; i++) {
+			let sample = Math.random();
+			if(sample >= p) { //failure
+				numFailure++;
+				boolArr.push('F');
+			} else { //success
+				numSuccess++;
+				boolArr.push('S');
+			}
+		}
+
+		$('#outputPara').html('Successes: ' + numSuccess + ' Failures: ' + numFailure + ' Success Percentage: ' + (numSuccess/(numSuccess+numFailure) ) );
+		$('#outputTextarea').val(boolArr);
+	});
+
+});
